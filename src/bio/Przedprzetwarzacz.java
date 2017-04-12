2e40161715a6c9d690b9d30b83c02de19cf645ab
student <student@LAB-GE-13.cs.put.poznan.pl>
2017-04-12 15:00:16 +0200

    :)

--- src/bio/Przedprzetwarzacz.java
+++ src/bio/Przedprzetwarzacz.java
@@ -2,6 +2,19 @@ package bio;
 
 class Przedprzetwarzacz {
 	private Oligonukleotyd[][] graf;
+	private Oligonukleotyd[] dane;
 	
+	public Oligonukleotyd[][]  generujGraf(Oligonukleotyd[] dane){
+		
+		
+		
+		return graf;
+	}
 	
+	private int policzOdleglosc(Oligonukleotyd lewy, Oligonukleotyd prawy){
+		String l=lewy.getLancuch();
+		String p=prawy.getLancuch();
+		
+		return 0;
+	}
 }
