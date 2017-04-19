97e69a6164fa973e11e59ccc124e90db42ca9d31
student <a@a.pl>
2017-04-19 14:13:28 +0200

    przedprzetwarzacz v0.1

--- src/bio/Przedprzetwarzacz.java
+++ src/bio/Przedprzetwarzacz.java
@@ -1,20 +1,61 @@
 package bio;
 
 class Przedprzetwarzacz {
-	private Oligonukleotyd[][] graf;
-	private Oligonukleotyd[] dane;
-	
-	public Oligonukleotyd[][]  generujGraf(Oligonukleotyd[] dane){
-		
-		
-		
+
+	public int[][]  generujGraf(Oligonukleotyd[] dane){
+		int n=dane.length;
+		int[][] graf=new int[dane.length][dane.length];
+		for (int i=0;i<n;i++) {
+			for (int j=0;j<n;j++) {
+				if(i==j)continue;
+				graf[i][j]=policzOdleglosc(dane[i], dane[j]);
+			}
+		}	
 		return graf;
 	}
 	
 	private int policzOdleglosc(Oligonukleotyd lewy, Oligonukleotyd prawy){
 		String l=lewy.getLancuch();
 		String p=prawy.getLancuch();
-		
-		return 0;
+		int L=l.length();
+		for(int i=1;i<L;i++){
+			if(l.substring(i).equals(p.substring(0, L-i-1))){
+				return i;
+			}
+		}
+		return L;
+	}
+	
+	public int[][] skrocGraf(int [][] graf, Oligonukleotyd[] dane){
+		int n=dane.length;
+		int indeks,indeks2;
+		for(int i=0;i<n;i++){
+			indeks=-1;
+			for(int j=0;j<n;j++){	
+				if(graf[i][j]==1){
+					if(indeks!=-1){
+						indeks=-1;
+						break;
+					}
+					indeks=j;
+				}
+			}
+			if(indeks!=-1){
+				indeks2=-1;
+				for(int k=0;k<n;k++){
+					if(graf[k][indeks]==1){
+						if(indeks2!=-1){
+							indeks2=-1;
+							break;
+						}
+						indeks2=k;
+					}
+				}
+				if(indeks2!=-1){
+					//TODO skracanie
+				}
+			}
+		}
+		return graf;
 	}
 }
