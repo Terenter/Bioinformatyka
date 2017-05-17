168c16dcf9f0a5f0ddb2818d854cb17c2757f361
Terenter <too.mek@wp.pl>
2017-05-17 14:46:20 +0200

    Merge

--- src/bio/Krzyzer.java
+++ src/bio/Krzyzer.java
@@ -8,10 +8,10 @@ import java.util.List;
 class Krzyzer {
 
 	public Rozwiazanie krzyzuj(Rozwiazanie r1, Rozwiazanie r2) {
-		r1.przeliczWartosc(Projekt.getGrafOl());
-		r2.przeliczWartosc(Projekt.getGrafOl());
-		int w1 = r1.getWartosc();
-		int w2 = r2.getWartosc();
+//		r1.przeliczWartosc(Projekt.getGrafOl());
+//		r2.przeliczWartosc(Projekt.getGrafOl());
+//		int w1 = r1.getWartosc();
+//		int w2 = r2.getWartosc();
 		int m = 0;
 		List<Oligonukleotyd> rTemp = new ArrayList<Oligonukleotyd>();
 		int i=0;
@@ -48,15 +48,15 @@ class Krzyzer {
 			}
 		}
 		//r1.setSlowa(rTemp);
-		Rozwiazanie Temp = new Rozwiazanie(rTemp);
-		Temp.przeliczWartosc(Projekt.getGrafOl());
-		System.out.println(Integer.toString(w1) + " " + Integer.toString(w2) + " " + Integer.toString(Temp.getWartosc()));
-		try {
-			System.in.read();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+//		Rozwiazanie Temp = new Rozwiazanie(rTemp);
+//		Temp.przeliczWartosc(Projekt.getGrafOl());
+//		System.out.println(Integer.toString(w1) + " " + Integer.toString(w2) + " " + Integer.toString(Temp.getWartosc()));
+//		try {
+//			System.in.read();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 		return new Rozwiazanie(rTemp);
 	}
 	
